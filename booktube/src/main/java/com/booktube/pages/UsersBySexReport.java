b3a61afc39d7485ca6d5fab116b037f2297e024d
robertarg72@gmail.com <robertarg72@gmail.com@08455545-65b1-6dad-fb82-5ee16c17f168>
2012-07-07 15:46:07 +0000

    Se hizo refactor de los reportes usando el Design Pattern: Template Method. Ahora es mas facil agregar mas reportes.

--- booktube/src/main/java/com/booktube/pages/UsersBySexReport.java
+++ booktube/src/main/java/com/booktube/pages/UsersBySexReport.java
@@ -5,33 +5,21 @@ package com.booktube.pages;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.request.resource.ContextRelativeResource;
-import org.apache.wicket.request.resource.IResource;
-import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.Dataset;
 
 import com.booktube.pages.utilities.BarReport;
-
+import com.booktube.pages.utilities.Report;
 import com.booktube.service.UserService;
 
 
 public class UsersBySexReport extends ReportPage {
 	private static final long serialVersionUID = -4994588390864291547L;
 	
-	protected AgeFilterOption ageFilter;
-	protected OriginFilterOption originFilter;
-	protected MiscFilterOption customizedMisc;
-	DropDownElementPanel genderDropDownElement;
-	private String[] labels = new String[]{"Evolución de Usuarios por Género", "Año", "Usuarios"};
-	
 	@SpringBean
-	UserService userService;
-	
+	UserService userService;	
 		
 	public UsersBySexReport(){
 		super();
@@ -47,54 +35,12 @@ public class UsersBySexReport extends ReportPage {
 		String newTitle = "Booktube - Users By Sex Report"; 
 		super.get("pageTitle").setDefaultModelObject(newTitle);	
 		
-		
-		// En esta clase se agrega el boton submit y el evento onSubmit pues cada Reporte
-		// necesitara informacion diferente y ejecutara graficos diferentes
-		form.add(new Button("renderReport", new Model<String>("Graficar")) {
-			private static final long serialVersionUID = 6743737357599494567L;
-			
-			@Override
-			public void onSubmit() {				
-				//List<?> data =  userService.getUserEvolutionByYear(originFilter, ageFilter, customizedMisc);
-				
-//				List<?> data  = getData();
-//				DefaultCategoryDataset dataset = new DefaultCategoryDataset();				
-//				for(Object object : data){
-//		           Map<?, ?> row = (Map<?, ?>)object;
-//		           String year = (String)row.get("year");
-//		           dataset.setValue((Integer)row.get("female"), "mujeres", year); 
-//		           dataset.setValue((Integer)row.get("male"), "hombres", year); 
-//		        }				
-
-				 
-			    int ANCHO_GRAFICA = 600;			    
-			    int ALTO_GRAFICA = 450;
-			    
-			    String filename = "src/main/webapp/img/report.png";
-			    
-			    try {
-			        final BarReport usersBySexReport = new BarReport(getData(), labels);
-			        usersBySexReport.saveReportAsPNG(filename, ANCHO_GRAFICA, ALTO_GRAFICA);			        			        
-			    } catch (Exception e) {
-			        e.printStackTrace();
-			    }		
-			    
-			    reportImage.setImageResourceReference(new ResourceReference(UsersEvolutionReport.class, "report.png") {
-					private static final long serialVersionUID = 7995864723435899261L;
-
-					@Override
-					public IResource getResource() {						
-						return new ContextRelativeResource("/img/report.png");
-					}
-				});
-			}
-		});
-
+		labels = new String[]{"Evolución de Usuarios por Género", "Año", "Usuarios"};	
 
 	}
 
 	@Override
-	public Dataset getData() {
+	public Dataset getReportData() {
 		List<?> data = userService.getUserEvolutionBySex(originFilter, ageFilter);
 		
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();				
@@ -106,6 +52,16 @@ public class UsersBySexReport extends ReportPage {
         }
 		return (Dataset)dataset;
 
+	}
+
+	@Override
+	public Report getReportType() {
+		return new BarReport(getReportData(), labels);
+	}
+
+	@Override
+	public Class<?> getReportClass() {
+		return UsersBySexReport.class;
 	}
 	
 }
